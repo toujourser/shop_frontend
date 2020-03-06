e439fd4c84ef27276e770ee3e5c5c31313153554
toujourser <toujourser@gmail.com>
2020-03-06 17:30:13 +0800

    login finish

--- src/main.js
+++ src/main.js
@@ -2,10 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+import './assets/css/global.css'
+import './assets/fonts/iconfont.css'
+import axios from 'axios';
 
-Vue.config.productionTip = false
+axios.defaults.baseURL = 'http://127.0.0.1:8081/api/v1.0';
+Vue.prototype.$http = axios; //将axios挂载到http上， 可以通过this.http 直接访问
+
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    render: h => h(App)
+}).$mount('#app');
